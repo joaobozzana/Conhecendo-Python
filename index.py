d28a34cc5b669b589cb7a393582d42d9c0998b0e
Joao Victor <joaoviicttor9@gmail.com>
2024-01-07 11:43:14 -0400

    Critério de senha e centralização das janelas

--- index.py
+++ index.py
@@ -6,15 +6,15 @@ import DataBase
 
 class IndexPage:
 
+    # Função de validar o login
     def logar(self):
         Usuario = self.UsuarioEntry.get()
         Senha = self.SenhaEntry.get()
 
         DataBase.cursor.execute("""
-            SELECT * FROM users 
+            SELECT * FROM usuarios 
             WHERE usuario = ? AND senha = ?
             """, (Usuario, Senha))
-        print("Foi")
         VerificaLogin = DataBase.cursor.fetchone()
         try:
             if (Usuario in VerificaLogin and Senha in VerificaLogin):
@@ -22,29 +22,33 @@ class IndexPage:
         except:
             messagebox.showerror(title="Informações de login", message="Acesso negado")
 
+    # Função pra chamar a janela de registro
     def registrar(self):
-        # Fechar a janela atual
         self.login.destroy()
-
-        # Adicione os widgets e configurações necessários para a nova janela aqui
         register = Register()
         register.registro.mainloop()
 
     def __init__(self):
         self.login = Tk()
-        self.login.title("Painel de login")
-        self.login.geometry("600x300")
-        self.login.configure(background="#E6E6E6")
-        self.login.resizable(width=False, height=False)
+        self.login.title("Painel de login") # Nome
+
+        # Obtém a resolução da tela
+        largura_tela = self.login.winfo_screenwidth()
+        altura_tela = self.login.winfo_screenheight()
+        # Calcula a posição para centralizar a janela
+        x_pos = (largura_tela - 600) // 2 
+        y_pos = (altura_tela - 300) // 2  
+
+        self.login.geometry(f"600x300+{x_pos}+{y_pos}")
+        self.login.configure(background="#E6E6E6") # Cor
+        self.login.resizable(width=False, height=False) # Proibe de aumentar/diminuir a janela
         self.login.attributes("-alpha", 0.9)  # Transparência da janela
 
         # IMG
         self.logo = PhotoImage(file="icons/logo.png")
 
-        # Widgets
         LeftFrame = Frame(self.login, width=200, height=300, bg="#BFBFBF", relief="solid", bd=2, borderwidth=1, padx=5, pady=5, highlightbackground="black", highlightcolor="black", highlightthickness=2)
         LeftFrame.pack(side=LEFT)
-
         RightFrame = Frame(self.login, width=400, height=300, bg="#BFBFBF", relief="solid", bd=2, borderwidth=1, padx=5, pady=5, highlightbackground="black", highlightcolor="black", highlightthickness=2)
         RightFrame.pack(side=RIGHT)
 
@@ -56,23 +60,21 @@ class IndexPage:
         LogoLabel = Label(LeftFrame, image=self.logo, bg="#BFBFBF")
         LogoLabel.place(x=40, y=80)
 
-        # User
+        # Usuario
         UsuarioLabel = Label(RightFrame, text="Usuário:", font=("Century Gothic", 14), bg="#BFBFBF", fg="black")
         UsuarioLabel.place(x=5, y=70)
-
         self.UsuarioEntry = ttk.Entry(RightFrame, width=30)
         self.UsuarioEntry.place(x=115, y=75)
-        self.UsuarioEntry.focus()  # Foca no campo de usuário inicialmente
+        self.UsuarioEntry.focus() 
 
         # Senha
         SenhaLabel = Label(RightFrame, text="Senha:", font=("Century Gothic", 14), bg="#BFBFBF", fg="black")
         SenhaLabel.place(x=5, y=120)
-
         self.SenhaEntry = ttk.Entry(RightFrame, width=30, show='●') 
         self.SenhaEntry.place(x=115, y=125)
 
         # Botoes
-        button_bg = "#4CAF50"  # Cor de destaque para os botões
+        button_bg = "#4CAF50"
 
         LoginButton = ttk.Button(RightFrame, text="Login", width=30, style='TButton', command=self.logar)
         LoginButton.place(x=70, y=200)
@@ -80,7 +82,6 @@ class IndexPage:
         RegistroButton = ttk.Button(RightFrame, text="Registrar-se", width=30, style='TButton', command=self.registrar)
         RegistroButton.place(x=70, y=240)
 
-        # Estilo para os botões
         style = ttk.Style()
         style.configure('TButton', font=('Century Gothic', 12), background=button_bg)
 
--- registro.py
+++ registro.py
@@ -1,18 +1,34 @@
 from distutils.util import execute
 from tkinter import Tk, Label, messagebox, ttk, Entry, PhotoImage
-from turtle import back
 import DataBase
+import re
 
 class Register:
 
-    def back(self):
-        # Fechar a janela atual
-        self.registro.destroy()
+    # Verificação da senha
+    def criterios_da_senha(self, senha):
+        # Pelo menos 6 caracteres
+        if len(senha) < 6:
+            return False
+        # Pelo menos 1 letra maiúscula
+        if not any(c.isupper() for c in senha):
+            return False
+        # Pelo menos 1 caracter especial
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', senha):
+            return False
+        # Pelo menos 1 número
+        if not any(c.isdigit() for c in senha):
+            return False
+        return True
 
-        from index import IndexPage  # Importa a classe IndexPage do arquivo index.py
+    # Função de voltar pra janela principal
+    def back(self):
+        self.registro.destroy()
+        from index import IndexPage 
         index_page = IndexPage()
         index_page.run()
 
+    # Função pra registrar novos usuarios e validar campos
     def RegistertoDataBase(self):
         Nome = self.NomeEntry.get()
         Email = self.EmailEntry.get()
@@ -28,22 +44,39 @@ class Register:
             campos_vazios.append("Usuário")
         if Senha == "":
             campos_vazios.append("Senha")
+        elif not self.criterios_da_senha(Senha):
+            campos_vazios.append("Senha não atende aos critérios")
 
         if campos_vazios:
             mensagem = f"Preencha os seguintes campos: {', '.join(campos_vazios)}"
             messagebox.showerror(title="Registro invalidado", message=mensagem)
         else:
             DataBase.cursor.execute("""
-                INSERT INTO users(nome, email, usuario, senha) VALUES(?, ?, ?, ?)
+                INSERT INTO usuarios(nome, email, usuario, senha) VALUES(?, ?, ?, ?)
             """, (Nome, Email, Usuario, Senha))
 
             DataBase.conn.commit()
             messagebox.showinfo(title="Informações de registro", message="Registro concluído")
 
+    def exibir_mensagem_senha(self, event):
+        self.CaixaMensagemSenha.config(text="A senha deve conter:\n- Letra maiúscula\n- Caracteres especiais\n- Números\n- Mínimo de 6 dígitos")
+
+    def esconder_mensagem_senha(self, event):
+        self.CaixaMensagemSenha.config(text="")
+
     def __init__(self):
         self.registro = Tk()
         self.registro.title("Painel de Registro")
-        self.registro.geometry("400x400")
+
+        # Obtém a resolução da tela
+        largura_tela = self.registro.winfo_screenwidth()
+        altura_tela = self.registro.winfo_screenheight()
+
+        # Calcula a posição para centralizar a janela
+        x_pos = (largura_tela - 450) // 2  # Assumindo que a largura da janela é 400
+        y_pos = (altura_tela - 400) // 2   # Assumindo que a altura da janela é 450
+
+        self.registro.geometry(f"450x400+{x_pos}+{y_pos}")
         self.registro.configure(background="#E6E6E6")
         self.registro.resizable(width=False, height=False)
         self.registro.attributes("-alpha", 0.9)  # Transparência da janela
@@ -57,31 +90,42 @@ class Register:
         self.LogoLabel = Label(self.registro, image=logo, bg="#E6E6E6")
         self.LogoLabel.place(x=180, y=10)
 
-        # Configurações adicionais, widgets, etc.
-
+        # Nome
         NomeLabel = Label(self.registro, text="Nome:", font=("Century Gothic", 14), bg="#E6E6E6")
         NomeLabel.place(x=15, y=70)
         self.NomeEntry = ttk.Entry(self.registro, width=30)
         self.NomeEntry.place(x=115, y=75)
         self.NomeEntry.focus()
 
+        # Email
         EmailLabel = Label(self.registro, text="Email:", font=("Century Gothic", 14), bg="#E6E6E6")
         EmailLabel.place(x=15, y=120)
         self.EmailEntry = ttk.Entry(self.registro, width=30)
         self.EmailEntry.place(x=115, y=125)
 
+        # Usuario
         UsuarioLabel = Label(self.registro, text="Usuário:", font=("Century Gothic", 14), bg="#E6E6E6")
         UsuarioLabel.place(x=15, y=170)
         self.UsuarioEntry = ttk.Entry(self.registro, width=30)
         self.UsuarioEntry.place(x=115, y=175)
         
+        # Senha
         SenhaLabel = Label(self.registro, text="Senha:", font=("Century Gothic", 14), bg="#E6E6E6")
         SenhaLabel.place(x=15, y=220)
         self.SenhaEntry = ttk.Entry(self.registro, width=30, show='●') 
         self.SenhaEntry.place(x=115, y=225)
 
+        # Caixa de mensagem para os critérios da senha
+        self.CaixaMensagemSenha = ttk.Label(self.registro, text="", font=("Century Gothic", 8), background="#E6E6E6", foreground="red")
+        self.CaixaMensagemSenha.place(x=315, y=235, anchor="w")
+
+        # Vincula a função exibir_mensagem_senha ao evento Enter do Entry
+        self.SenhaEntry.bind("<Enter>", self.exibir_mensagem_senha)
+        # Vincula a função esconder_mensagem_senha ao evento Leave do Entry
+        self.SenhaEntry.bind("<Leave>", self.esconder_mensagem_senha)
+
         # Botoes
-        button_bg = "#4CAF50"  # Cor de destaque para os botões
+        button_bg = "#4CAF50" 
 
         RegistroButton = ttk.Button(self.registro, text="Registrar", width=30, style='TButton', command=self.RegistertoDataBase)
         RegistroButton.place(x=70, y=280)
@@ -89,7 +133,6 @@ class Register:
         VoltarButton = ttk.Button(self.registro, text="Voltar", width=30, style='TButton', command=self.back)
         VoltarButton.place(x=70, y=320)
 
-        # Estilo para os botões
         style = ttk.Style()
         style.configure('TButton', font=('Century Gothic', 12), background=button_bg)
 
